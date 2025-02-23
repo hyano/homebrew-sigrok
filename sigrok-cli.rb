d36068b53a0459dfc661b73fd70d9f8599e8244e
hyano <1297631+hyano@users.noreply.github.com>
2025-02-24 04:49:13 +0900

    add head for sigrok-cli and sigrok-firmware-fx2lafw

--- sigrok-cli.rb
+++ sigrok-cli.rb
@@ -4,8 +4,11 @@ class SigrokCli < Formula
   desc "Command-line frontend for sigrok"
   homepage "https://sigrok.org/wiki/Sigrok-cli"
   url "https://sigrok.org/download/source/sigrok-cli/sigrok-cli-0.7.2.tar.gz"
+  head "git://sigrok.org/sigrok-cli"
   sha256 "71d0443f36897bf565732dec206830dbea0f2789b6601cf10536b286d1140ab8"
 
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
   depends_on "glib" => :build
   depends_on "make" => :build
   depends_on "pkg-config" => :build
@@ -13,6 +16,7 @@ class SigrokCli < Formula
   depends_on "hyano/sigrok/libsigrokdecode"
 
   def install
+    system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
--- sigrok-firmware-fx2lafw.rb
+++ sigrok-firmware-fx2lafw.rb
@@ -5,12 +5,16 @@ class SigrokFirmwareFx2lafw < Formula
   desc "Open-source firmware for Cypress FX2 chips"
   homepage "https://sigrok.org/wiki/Fx2lafw"
   url "https://sigrok.org/download/source/sigrok-firmware-fx2lafw/sigrok-firmware-fx2lafw-0.1.7.tar.gz"
+  head "git://sigrok.org/sigrok-firmware-fx2lafw"
   sha256 "a3f440d6a852a46e2c5d199fc1c8e4dacd006bc04e0d5576298ee55d056ace3b"
 
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
   depends_on "make" => :build
   depends_on "sdcc" => :build
 
   def install
+    system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
